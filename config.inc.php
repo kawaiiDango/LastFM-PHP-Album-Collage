062cce369e22fc92e437403fd8f7872092597d58
kawaiiDango <1066519+kawaiiDango@users.noreply.github.com>
2020-03-01 22:16:05 +0530

    Delete config.inc.php

--- config.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-$config['accessKey'] = '<AccessKey>';
-$config['secretKey'] = '<secretKey>';
-$config['api_key'] = '<Lastfm API key>';
-$config['debug'] = true;
-$config['bucket'] = '<BucketName>';
-*/
-?>
